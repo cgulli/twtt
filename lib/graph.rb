3ae7e26870a782875c09717d4d172cd363844ba0
cgulli <caitlin.gulliford@gmail.com>
2016-12-08 17:17:22 +0000

    created graph class and wrote test for parse n build function to make a graph with data from txt file

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,5 +1,20 @@
 class Graph
+attr_accessor :edges
 
+def initialize(testinput=nil)
+  @edges = []
 
+end
+
+def parse_n_build(testinput)
+
+  File.read(testinput).scan(/\w\w\d+/).each do |line|
+    start = line[0,1]
+    finish = line[1,1]
+    weight = line[2..-1].to_i
+    edge = Edge.new(start, finish, weight)
+    @edges << edge
+  end
+end
 
 end
--- spec/graph_spec.rb
+++ spec/graph_spec.rb
@@ -1,36 +1,36 @@
 require 'graph'
-require 'edge'
+# require 'edge'
 
 describe Graph do
 
-  context '#initialize'do
-  graph = Graph.new
-    it 'stores a collection of nodes/stations' do
-      expect(graph.nodes).to be_empty
+  context '#building a graph' do
+  # graph = Graph.new
+    it 'reads a text file and creates edges' do
+      expect{subject.parse_n_build('testinput.txt')}.to change{subject.edges.count}.from(0).to(9)
     end
   end
 
-  context '#creates routes' do
-    graph = Graph.new
-    it 'adds nodes to array' do
-      expect(graph).to respond_to(:add_node).with(1).argument
-    end
-    it 'adds an edge' do
-      expect(graph).to respond_to(:add_edge).with(3).arguments
-      #refactor into a better test
-    end
-  end
-
-  context '#manages nodes' do
-    graph = Graph.new
-    graph.add_node('A')
-    graph.add_node('B')
-    it 'finds nodes by name' do
-      respond_to(:find_node).with(1).argument
-      #refactor into a better test
-    end
-    it 'counts nodes' do
-      respond_to(:node_count).with(2)
-    end
-  end
+  # context '#creates routes' do
+  #   graph = Graph.new
+  #   it 'adds nodes to array' do
+  #     expect(graph).to respond_to(:add_node).with(1).argument
+  #   end
+  #   it 'adds an edge' do
+  #     expect(graph).to respond_to(:add_edge).with(3).arguments
+  #     #refactor into a better test
+  #   end
+  # end
+  #
+  # context '#manages nodes' do
+  #   graph = Graph.new
+  #   graph.add_node('A')
+  #   graph.add_node('B')
+  #   it 'finds nodes by name' do
+  #     respond_to(:find_node).with(1).argument
+  #     #refactor into a better test
+  #   end
+  #   it 'counts nodes' do
+  #     respond_to(:node_count).with(2)
+  #   end
+  # end
 end
