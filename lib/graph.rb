aa5a903f6fccc1da4376561fdcb6b8b286073dd3
cgulli <caitlin.gulliford@gmail.com>
2016-12-12 17:18:53 +0000

    final version no working no of trips function or shortest distance function

--- lib/graph.rb
+++ lib/graph.rb
@@ -29,9 +29,50 @@ attr_accessor :edges
   end
 
   def num_of_trips(start, finish, opts= {})
-
+    opts = { :min_stops => 1, :max_stops => Infinity, :max_distance => Infinity}.merge(opts)
+    total = 0
+    explore_paths(start, finish, opts) { total += 1}
+    total
   end
 
+# This is obviously where I got stuck but this gives a sense of where I was going...
+
+#     def explore_paths(start, finish, opts, stats = {:distance => 0, :stops => 0}, &callback)
+#       lower_bound_ok = stats[:stops] >= opts[:min_stops]
+#       upper_bound_ok = stats[:distance] <= opts[:max_distance] && stats[:stops] <= opts[:max_stops]
+# #look up currying eg.call,bind
+#
+#       callback.call(stats[:path]) if start == finish && lower_bound_ok && upper_bound_ok
+#       # Recursion basis: stop exploration when reached upper-bound restrictions
+#       if upper_bound_ok
+#         @edges[egde.start].each do |nxt, dist|
+#           inner_stats = {:distance => stats[:distance] + dist, :stops => stats[:stops] + 1}
+#           explore_paths(nxt, finish, opts, inner_stats, &callback)
+#         end
+#       end
+#     end
+
+# count_Trips(start, finish , ...)
+# count_Trips(finish, start , ...)
+#
+#
+# funtion count_Trips(current, next, origin=current, stats, opts, callback)
+# #current = a
+# #next = n
+# #[edgeobjid1{start, finish, weight}, edgeobjid1{start, finish, weight}...]
+#
+#
+# for edge in edges:
+#   if edge.start == current #find edges that start with current
+#   	does edge.finish == next ? if yes and options are OK then increment this trip to total ie.callback
+# 	if not: count_trips(edge.finish, next) # increment stops
+
+  #if no edges.start start with current then we have exhausted and found no trips or no node of origin
+
+
+# above example functions graph model: {nodea:{nodeb:7, noder:3}, nodeb:{noded:3, noder:4}}
+
+#look for array pairs that start with given start node then do next?; look for pairs that finish with given finish node then do prev?
 
   def shortest_dis(start, finish)
 
